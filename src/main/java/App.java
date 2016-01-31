1daeb28c828d791fd30bfb80a7ff873f7ce4e2b5
Mateus Lohn Andriani <mtslohn@gmail.com>
2016-01-31 18:29:36 -0200

    Recuperação das IRIs dos termos filhos da IRI passada na consulta

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,27 +1,51 @@
 import java.io.InputStream;
 
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.util.FileManager;
 
-
 public class App {
-	
+
 	private static final String CATEGORY_LABELS = "category-labels_pt.ttl",
-			ARTICLE_CATEGORIES = "skos-categories_pt.ttl";
+			SKOS_CATEGORIES = "skos-categories_pt.ttl";
 
 	public static void main(String[] args) {
 		
+		Model model = loadModel();
+
+		String queryString = "PREFIX dbc:<http://purl.org/dc/terms/>"
+						+ "PREFIX skos:<http://www.w3.org/2004/02/skos/core#>"
+						+ "SELECT ?broader WHERE {"
+						+ "?broader skos:broader <http://pt.dbpedia.org/resource/Categoria:Brasil> ."
+						+ "} ";
+		
+		Query query = QueryFactory.create(queryString);
+		
+		ResultSet rs = QueryExecutionFactory.create(query, model).execSelect();
+		
+		while (rs.hasNext()) {
+			QuerySolution solution = rs.nextSolution();
+			System.out.println(solution);
+		}
+		
+	}
+
+	private static Model loadModel() {
+
 		Model model = ModelFactory.createDefaultModel();
 
 		InputStream labelsStream = FileManager.get().open(CATEGORY_LABELS);
-		InputStream hierarchyStream = FileManager.get().open(ARTICLE_CATEGORIES);
-		
+		InputStream hierarchyStream = FileManager.get().open(SKOS_CATEGORIES);
+
 		model.read(labelsStream, null, "TURTLE");
 		model.read(hierarchyStream, null, "TURTLE");
-		
-		model.write(System.out);
-		
+		return model;
+
 	}
 
 }
