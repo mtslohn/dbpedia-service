fbb2f3dddc072902f6bb623f3598af678a6719c5
Mateus Lohn Andriani <mtslohn@gmail.com>
2016-05-08 02:16:10 -0300

    [WIP] Utilização de Algebra ARQ para a consulta de termos-pai
    -Ganho de performance no processamento chega a 1600%;
    -Eliminação do parsing de texto de entrada da consulta;
    -Validação inicial do resultado obtido.

--- src/main/java/br/ufsc/egc/dbpedia/reader/service/DBPediaService.java
+++ src/main/java/br/ufsc/egc/dbpedia/reader/service/DBPediaService.java
@@ -4,8 +4,9 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.print.attribute.standard.Severity;
-
+import org.apache.jena.graph.NodeFactory;
+import org.apache.jena.graph.Triple;
+import org.apache.jena.query.ParameterizedSparqlString;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
@@ -15,9 +16,16 @@ import org.apache.jena.query.Syntax;
 import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.sparql.algebra.Algebra;
+import org.apache.jena.sparql.algebra.Op;
+import org.apache.jena.sparql.algebra.op.OpBGP;
+import org.apache.jena.sparql.core.BasicPattern;
+import org.apache.jena.sparql.core.ResultBinding;
+import org.apache.jena.sparql.core.Var;
+import org.apache.jena.sparql.engine.QueryIterator;
+import org.apache.jena.sparql.engine.ResultSetStream;
 import org.apache.jena.util.FileManager;
 import org.apache.log4j.Logger;
-import org.apache.log4j.Priority;
 
 import br.ufsc.egc.curriculumextractor.model.taxonomy.Term;
 
@@ -110,22 +118,53 @@ public class DBPediaService {
 		return narrowConcepts;
 
 	}
+	
+	
+	//TODO refactoring
+//	private ParameterizedSparqlString sparqlString = null;
 
 	public List<String> findBroaderConcepts(String conceptName) {
+		
+		BasicPattern basicPattern = new BasicPattern();
+		
+		Var varConcept = Var.alloc("concept");
+		Var varBroader = Var.alloc("broader");
+		Var varBroaderLabel = Var.alloc("broaderLabel");
+		
+		final String RDFS_URI = "http://www.w3.org/2000/01/rdf-schema#";
+		final String SKOS_URI = "http://www.w3.org/2004/02/skos/core#";
+		
+		basicPattern.add(new Triple(varConcept, NodeFactory.createURI(RDFS_URI + "label"), NodeFactory.createLiteral(conceptName, "pt")));
+		basicPattern.add(new Triple(varConcept, NodeFactory.createURI(SKOS_URI + "broader"), varBroader));
+		basicPattern.add(new Triple(varBroader, NodeFactory.createURI(RDFS_URI + "label"), varBroaderLabel));
+		
+		Op op = new OpBGP(basicPattern);
+		
+		QueryIterator queryIterator = Algebra.exec(op, model);
+		
+		List<String> resultVars = new ArrayList<String>();
+		resultVars.add("broaderLabel");
+		ResultSet rs = new ResultSetStream(resultVars , model, queryIterator);
+		
+//		if (sparqlString == null) {
+//			sparqlString = new ParameterizedSparqlString();
+//
+//			sparqlString.append("PREFIX dbc:<http://purl.org/dc/terms/> "
+//					+ "PREFIX skos:<http://www.w3.org/2004/02/skos/core#> "
+//					+ "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> "
+//					+ "SELECT ?broaderLabel WHERE { " 
+//					+ "?concept rdfs:label ?conceptName . "
+//					+ "?concept skos:broader ?broader . "
+//					+ "?broader rdfs:label ?broaderLabel " + "} ");
+//		}
+//		
+//		sparqlString.setLiteral("conceptName", conceptName, "pt");
 
-		String queryString = "PREFIX dbc:<http://purl.org/dc/terms/> "
-				+ "PREFIX skos:<http://www.w3.org/2004/02/skos/core#> "
-				+ "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> "
-				+ "SELECT ?broaderLabel WHERE { " + "?concept rdfs:label \""
-				+ conceptName + "\"@pt . "
-				+ "?concept skos:broader ?broader . "
-				+ "?broader rdfs:label ?broaderLabel " + "} ";
+//		LOGGER.debug(sparqlString.toString());
 
-		LOGGER.debug(queryString);
+//		Query query = sparqlString.asQuery();
 
-		Query query = QueryFactory.create(queryString);
-
-		ResultSet rs = QueryExecutionFactory.create(query, model).execSelect();
+//		ResultSet rs = QueryExecutionFactory.create(query, model).execSelect();
 
 		List<String> broaderConcepts = new ArrayList<String>();
 
@@ -138,6 +177,8 @@ public class DBPediaService {
 			}
 			broaderConcepts.add(literal.getString());
 		}
+		
+		queryIterator.close();
 
 		return broaderConcepts;
 
